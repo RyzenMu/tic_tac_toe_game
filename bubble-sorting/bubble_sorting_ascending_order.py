4967cba07a700e92fdce4edf2e43ca70083793b8
RyzenMu <muruga13in2022@gmail.com>
2022-07-04 21:30:31 +0530

    bubble sorting easy

---
+++ bubble-sorting/bubble_sorting_ascending_order.py
@@ -0,0 +1,16 @@
+list_a = [4, 7, 1, 3]
+
+
+
+for i in range(len(list_a)):
+    for j in range(len(list_a)):
+        print(i, j)
+        if list_a[i] == list_a[j]:
+            continue
+        elif list_a[i] > list_a[j]:
+            continue
+        else:
+            list_a[i], list_a[j] = list_a[j], list_a[i]
+        print(list_a)
+        print('------------------')
+
